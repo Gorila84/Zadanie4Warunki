714990bf60f80f647bb8dc140dce92debe746823
Maciej Sikora <maciek.sikora84@gmail.com>
2024-01-24 19:48:34 +0100

    Add project files.

---
+++ Zadanie4Warunki/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Podaj rok:");
+int year;
+var enteredYear = Console.ReadLine();
+Int32.TryParse(enteredYear, out year);
+
+if((year % 4  == 0 && year % 100 !=0) || year % 400 ==0)
+{
+    Console.WriteLine($"Rok {year} jest rokiem przestępnym");
+}
+else
+{
+    Console.WriteLine($"Rok {year} nie jest rokiem przestępnym");
+}
